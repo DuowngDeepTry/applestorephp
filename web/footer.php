4c36b96d58f30bf20972334c656085500ab29e65
C:/Program Files/Git/DUONGVU <duonghutech26@gmail.com>
2023-03-08 21:01:46 +0700

    Them giao dien basic

---
+++ web/footer.php
@@ -0,0 +1,5 @@
+<div class="clear"></div>
+        <div class="footer">
+            <p class="footer_c">Email: duonghutech26@gmail.com</p>
+            <p class="footer_c">SDT: 0865299626</p>
+        </div>
